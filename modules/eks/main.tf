8552fe7c496b5a37dcb179ce47f79048361b7bf0
Lakshmi Narayana Nunna <devops@bluebash.co>
2026-09-21 21:33:31 +0530

    chnaged authentication for cluster

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -84,7 +84,8 @@ resource "aws_eks_cluster" "this" {
   version  = var.kubernetes_version
 
   access_config {
-    authentication_mode = "API_AND_CONFIG_MAP"
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
   }
 
   vpc_config {
