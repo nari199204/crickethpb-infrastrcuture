87c1298b94ad789a80b2feafeb97a4de6927bc8b
Lakshmi Narayana Nunna <devops@bluebash.co>
2026-09-21 18:58:54 +0530

    fix authentication mode of eks cluster

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -83,6 +83,10 @@ resource "aws_eks_cluster" "this" {
   role_arn = aws_iam_role.cluster.arn
   version  = var.kubernetes_version
 
+  access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+  }
+
   vpc_config {
     subnet_ids              = var.subnet_ids
     endpoint_private_access = var.endpoint_private_access
